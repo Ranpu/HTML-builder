e01cd170c802b69c85deab00bff40e6b69398587
Aleksandr Mikhaylov <alex.mcgee.mail@gmail.com>
2022-11-06 22:42:27 +0300

    feat: finish 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const fs = require('fs/promises');
+const path = require('path');
 
+const originalDir = 'files';
+const NewDir = 'files-copy';
+
+const pathToOriginalDir = path.join(__dirname, originalDir);
+const pathToNewDir = path.join(__dirname, NewDir);
+
+const makeNewDir = async (pathToNewDir) => {
+  await fs.rm(pathToNewDir, { recursive: true, force: true });
+  await fs.mkdir(pathToNewDir, { recursive: true });
+}
+
+const copyFiles = async (pathToOriginalDir, pathToNewDir) => {
+  const files = await fs.readdir(pathToOriginalDir);
+
+  files.forEach(file => {
+    fs.copyFile(path.join(pathToOriginalDir, file), path.join(pathToNewDir, file));
+  });
+}
+
+const main = async () => {
+  await makeNewDir(pathToNewDir);
+  await copyFiles(pathToOriginalDir, pathToNewDir);
+  console.log('Done!');
+}
+
+main();
